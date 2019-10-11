b6366371a707399a6fbbb5a65c2140f13fa39dd9
Madhumitha <43128631+madhumitha998@users.noreply.github.com>
2019-10-11 17:28:09 +0800

    Added method countTrumpAndHigherCards( )

--- Computer.java
+++ Computer.java
@@ -1,11 +1,39 @@
+import java.util.ArrayList;
+
 public class Computer extends Player{
 
     public Player(id,position){
         super(id,position);
     }
 
-    private int countTrumpAndHigherCards(){
+    private double countTrumpAndHigherCards(Hand myHand){
+        List<String> namesList = Arrays.asList( "a", "q", "k","j","10","9");
+        ArrayList<String> HIGH_VALUES = new ArrayList<>();
+        HIGH_VALUES.addAll(namesList); 
+        
+        //Gets trump suit for current round
+        //String trumpSuitName = gameLogic.trumpCard.suitToString().
+        //but for now i will create a dummy value for testing
+        String trumpSuitName = "Hearts";
+        int trumpAndHigherCardsCnt = 0;
 
+        int totalCardsInHand=myHand.getNumberOfCards();
+        for(int i=0;i<totalCardsInHand;i++){
+            Card c = myHand.getCard(i);
+            
+            //check if trump suit 
+            if(c.suitToString()==trumpSuitName){
+                trumpAndHigherCardsCnt+=1;
+            }
+
+            //check if card is High value 
+            if (HIGH_VALUES.contains(c.getRank().getSymbol()) ) {
+                trumpAndHigherCardsCnt+=1;
+            }
+
+        }
+        //check how to give exact percentage without rounding
+        return trumpAndHigherCardsCnt/totalCardsInHand;
     }
 
     public void bidWinningTricks(){
