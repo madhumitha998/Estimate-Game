6ec8788ec8c22c8ead58f54194519577b246a3fd
abelwong2017 <abel.wong.2017@sis.smu.edu.sg>
2019-10-13 20:00:55 +0800

    Implement Game Logic And Some Methods

---
+++ src/helperFunctions/MapUtil.java
@@ -0,0 +1,13 @@
+public class MapUtil {
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Entry.comparingByValue());
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+}
