4a2b0ebe7ab31bd8b376fa5bc7ec21d9fe5396ca
Elias <lkmelias@gmail.com>
2019-10-15 19:04:24 +0800

    Add scoreboard class

---
+++ src/estimate/scoreboard/ScoreBoard.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class ScoreBoard{
+	private ArrayList<Rounds> scoreBoard;
+
+	Dictionary TotalScore = new HashTable();
+
+}
