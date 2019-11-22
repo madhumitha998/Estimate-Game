909b05374d9ffd93b230494e5a1971c0449ecd64
Abel Wong <abel.wong.2017@sis.smu.edu.sg>
2019-11-22 23:17:49 +0800

    Add Java Docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cards"},{"l":"estimate.gamelogic"},{"l":"estimate.player"},{"l":"estimate.scoreboard"},{"l":"estimate.view"}]
